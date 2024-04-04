6c4eb1c9e80b8b412268d15f3f57b5855ef3696d
dashutkabramovich <guseva.darya@mail.ru>
2024-04-04 23:43:22 +0300

    lesson3

---
+++ Lesson3/lesson_3_task_2.py
@@ -0,0 +1,15 @@
+from smartphone import Smartphone
+
+phone1 = Smartphone
+phone2 = Smartphone
+phone3 = Smartphone
+phone4 = Smartphone
+phone1 = ("Apple", "15 Pro", "+7666666666")
+phone2 = ("Honor", "X8", "+79031411414")
+phone3 = ("Oppo", "Find N3 Flip", "+79091112324")
+phone4 = ("Samsung", "Note9", "+79859855858")
+
+catalog = [phone1, phone2, phone3, phone4]
+
+for phones in catalog:
+    print(phones)
